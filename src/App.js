4953d524462657f24454802f9eff641a54cf8804
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-06-01 15:22:57 +0200

    Add not found component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bulma/css/bulma.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <HashRouter basename="/">
+        <Switch>
+          <Route path="/" component={NotFound} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
