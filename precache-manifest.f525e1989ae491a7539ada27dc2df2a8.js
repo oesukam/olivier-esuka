519e065a79cdc281536563fe5e94652a9e6e9be6
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-06-01 14:28:57 +0200

    Updates

---
+++ precache-manifest.f525e1989ae491a7539ada27dc2df2a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df35ca7f9fff7f0dabdfcd0ad3b242f9",
+    "url": "/olivier-esuka/index.html"
+  },
+  {
+    "revision": "61e194684f3788dca3c3",
+    "url": "/olivier-esuka/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/olivier-esuka/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "61e194684f3788dca3c3",
+    "url": "/olivier-esuka/static/js/main.eed58b00.chunk.js"
+  },
+  {
+    "revision": "0df1c9cb4126b1947aa5",
+    "url": "/olivier-esuka/static/js/runtime~main.7d670cea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/olivier-esuka/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/olivier-esuka/precache-manifest.f525e1989ae491a7539ada27dc2df2a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/olivier-esuka/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
